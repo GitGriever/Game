1794b93006ab8f6eab5380b724be305679866507
Lei <keishadaco@gmail.com>
2020-03-30 16:16:39 -0700

    new stufffff

---
+++ LCD_Nokia.h
@@ -0,0 +1,51 @@
+// Nokia 5110 LCD (PCD8544)
+// datasheet found at
+// https://www.sparkfun.com/datasheets/LCD/Monochrome/Nokia5110.pdf
+
+#include <stdbool.h>
+
+#define MAX_X	84
+#define MAX_Y	48
+
+#define LCDW     84
+#define LCDH     48
+
+void LCD_init(void);
+
+void LCD_clear(void);
+
+void LCD_clearBuffer(void)
+
+void LCD_DisplayBuffer(void);
+
+void LCD_SaveLastBuffer(void);
+
+void LCD_char(unsigned char data);
+
+void LCD_string(char *ptr);
+
+void LCD_uDec(char *ptr);
+
+void LCD_setCursor(unsigned char newX, unsigned char newY);
+
+void LCD_fullImage(const char *ptr);
+
+void LCD_bmp(unsigned char xval, unsigned char yval, const unsigned char *ptr, unsigned char threshold);
+
+void LCD_setPixel(unsigned char x, unsigned char y);
+
+void LCD_clearPixel(unsigned char x, unsigned char y);
+
+bool LCD_askPixel(unsigned char x, unsigned char y);
+
+bool LCD_askLastPixel(unsigned char x, unsigned char y);
+
+void LCD_char_4x4pix(unsigned char data);
+
+void LCD_string_4x4pix(char *ptr);
+
+void LCD_char_4x4pix_toBuffer(unsigned char xval, unsigned char yval, unsigned char data);
+
+void LCD_string_4x4pix_toBuffer(unsigned char xval, unsigned char yval, char *ptr);
+
+void LCD_uDec_4x4pix_toBuffer(unsigned char xval, unsigned char yval, unsigned short n);
--- frame.c
+++ frame.c
@@ -5,13 +5,13 @@ void HUD(void)
 	{
 		LCD_SetPixel(i, SCREENH - 43); // need to adjust slide pot to be between upper HUD and ground
 	}
-	LCD_OutStr(0, SCREENH - 48, "Score:"); 
-	LCD_OutUDec(30, SCREENH - 48, player.score);
-	LCD_OutStr(50, SCREENH - 48, "Best:"); 
-	LCD_OutUDec(65, SCREENH - 48, player.bestScore);
+	LCD_string(0, SCREENH - 48, "Score:"); 
+	LCD_uDec(30, SCREENH - 48, player.score);
+	LCD_string(50, SCREENH - 48, "Best:"); 
+	LCD_uDec(65, SCREENH - 48, player.bestScore);
 
-	LCD_OutStr(0, SCREENH - 7, "----------------"); 	// separates the ground
-	LCD_OutStr(0, SCREENH - 4, "////////////////"); 	// screen only displays 16 characters
+	LCD_string(0, SCREENH - 7, "----------------"); 	// separates the ground
+	LCD_string(0, SCREENH - 4, "////////////////"); 	// screen only displays 16 characters
 
 	// Nokia is a 48x84 LCD
 	// Range of SCREENW is 0(left)-82(right)
